f52a33bbc8fc706c92e59801ca0f26f00d16f64a
NEULOVIM <neulovim999@mail.ru>
2024-03-02 13:19:24 +0300

    DZ LESSON3

---
+++ src/lesson3/_Cikl/Task3.java
@@ -0,0 +1,20 @@
+package lesson3._Cikl;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        int g = 7;
+        while (g <= 99) {
+            System.out.println(g);
+            g ++;
+            g ++;
+            g ++;
+            g ++;
+            g ++;
+            g ++;
+            g ++;
+// у меня не хочит работать при g + 7;
+        }
+    }
+}
+
