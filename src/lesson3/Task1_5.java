257719b1a5c9c38b1bd08985ad3f8374a6c18e9a
NEULOVIM <neulovim999@mail.ru>
2024-03-02 15:58:23 +0300

    DZ LESSON3

---
+++ src/lesson3/Task1_5.java
@@ -0,0 +1,40 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class Task1_5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите число от 1 до 7");
+        String w = sc.nextLine();
+
+        switch (w) {
+            case "1":
+                System.out.println("Красный");
+                break;
+            case "2":
+                System.out.println("Оранжевый");
+                break;
+            case "3":
+                System.out.println("Жёлтый");
+                break;
+            case "4":
+                System.out.println("Зелёный");
+            case "5":
+                System.out.println("Голоубой");
+                break;
+            case "6":
+                System.out.println("Синий");
+            case "7":
+                System.out.println("Фиолетовый");
+                break;
+            default:
+                System.out.println("Неправелбное число");
+
+        }
+
+    }
+}
+
