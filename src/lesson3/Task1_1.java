c0f662c2a78b08017d3142ea9d38683cad26d3d1
NEULOVIM <neulovim999@mail.ru>
2024-03-04 16:56:56 +0300

    DZ LESSON3

--- src/lesson3/Task1_1.java
+++ src/lesson3/Task1_1.java
@@ -7,45 +7,30 @@ public class Task1_1 {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Введите число любого месяца");
-        String a = sc.nextLine();
 
-        switch (a) {
-            case "1":
+        System.out.println("Введите число любого месяца");
+        int b = sc.nextInt();
+
+        switch (b) {
+            case 12:
+            case 1:
+            case 2:
                 System.out.println("Зима");
                 break;
-            case "2":
-                System.out.println("Зима");
-                break;
-            case "3":
+            case 3:
+            case 4:
+            case 5:
                 System.out.println("Весна");
                 break;
-            case "4":
-                System.out.println("Весна");
-                break;
-            case "5":
-                System.out.println("Весна");
-                break;
-            case "6":
+            case 6:
+            case 7:
+            case 8:
                 System.out.println("Лето");
                 break;
-            case "7":
-                System.out.println("Лето");
-                break;
-            case "8":
-                System.out.println("Лето");
-                break;
-            case "9":
+            case 9:
+            case 10:
+            case 11:
                 System.out.println("Осень");
-                break;
-            case "10":
-                System.out.println("Осень");
-                break;
-            case "11":
-                System.out.println("Осень");
-                break;
-            case "12":
-                System.out.println("Зима");
                 break;
             default:
                 System.out.println("Число не подходит!");
